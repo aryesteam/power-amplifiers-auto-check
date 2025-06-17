5ace024d64f94f19869285bf439f8c5039a26999
teamgilaad <teamgilaad@gmail.com>
2025-06-17 15:29:45 +0300

    add state rolling to children

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { getStockDevices, getOperationCommand } from "./requests/requests.js";
 import HomePage from "./pages/HomePage/HomePage";
-import NextPage from "./pages/NextPage/NextPage";  
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  
+import NextPage from "./pages/NextPage/NextPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [data, setData] = useState({});
+  const [appData, setAppData] = useState({});
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchMyData = async () => {
       const stock = await getStockDevices();
       const oc = await getOperationCommand();
-      setData({ ...data, oc: oc, stock: stock });
+      setAppData({ ...appData, oc: oc, stock: stock });
       setLoading(false);
     };
     fetchMyData();
   }, []);
 
-  if (loading) return <div>טוען נתונים...</div>;
+  if (loading) return <div>loading data...</div>;
 
+  console.log(appData);
 
   return (
     <Router>
       <>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-        
+          <Route path="/" element={<HomePage data={setAppData} />} />
           <Route path="/next-page" element={<NextPage />} />
           {/* אפשר להוסיף כאן עוד Route's לדפים נוספים */}
         </Routes>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,19 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function HomePage() {
-  const navigate = useNavigate(); 
-  function goToNextPage() {
-    navigate("/next-page"); 
-  }
-  
+function HomePage(params) {
+  const { data } = params;
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>ברוכים הבאים לעמוד הבית</h1>
-      <button onClick={goToNextPage}>
-        לחץ כאן כדי לעבור לעמוד הבא 
+      <button
+        onClick={() => {
+          data({ test: "a" });
+          navigate("/next-page");
+        }}
+      >
+        לחץ כאן כדי לעבור לעמוד הבא
       </button>
     </div>
   );
